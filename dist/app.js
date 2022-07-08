ce30338c0f1fade26cc67f0e2934c315a75c917d
Niko <soder.n@gmail.com>
2022-07-08 19:03:29 +0300

    get quiz data

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,19 @@
 "use strict";
 const API_URL = 'https://opentdb.com/api.php?amount=5&type=multiple';
+// get quiz data
+async function getData() {
+    const response = await fetch(API_URL);
+    return await response.json();
+}
+function useData(data) {
+    const questions = data.results; // get all questions
+    console.log(questions);
+}
+// main
+function main() {
+    const triviaData = getData();
+    triviaData.then(function (triviaData) {
+        useData(triviaData);
+    });
+}
+main();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,45 @@
 const API_URL: string = 'https://opentdb.com/api.php?amount=5&type=multiple';
+
+// get quiz data
+async function getData(): Promise<QuestionList> {
+    const response = await fetch(API_URL);
+    return await response.json();
+}
+
+function useData(data: QuestionList) {
+    const questions = data.results;  // get all questions
+    console.log(questions);
+}
+
+// main
+function main() {
+    const triviaData = getData();
+    triviaData.then(function(triviaData) {
+        useData(triviaData);
+    });
+}
+main();
+
+
+
+
+
+
+// typescript interfaces
+interface QuestionList {
+    response_code:number;
+    results: {
+        category: string;
+        type: string;
+        difficulty: string;
+        question: string;
+        correct_answer: string;
+        incorrect_answers: string[];
+    }[];
+}
+
+
+
+
+
+
